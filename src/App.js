cdd58789ba953b34f3fe894885b3b45db3ab28be
Jonathan <jonathankwon@yahoo.com>
2022-05-10 14:57:35 -0700

    use axios to fetch Pokemon API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
+import axios from 'axios'
+
 
 function App() {
+  //      State     SetState      default State
+  const [pokemon, setPokemon] = useState([]);
+
+  // axios method
+  const axiosPokemon = () => {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0")
+      .then( response => {
+        // console.log(response.data);
+        setPokemon(response.data.results);
+      })
+      .catch( err => console.log(err))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Names of 807 Pokemon</h1>
+      <h2>Using axios</h2>
+      <button onClick={axiosPokemon}>Fetch Pokemon</button>
+      
+      {pokemon.map((poke, i) => (<li key={i}>{poke.name}</li>))}
     </div>
   );
 }
